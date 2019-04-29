ec7df3ddcbdab2cf693ce7fe4e512d32e40e46a6
Alex Chambers <alexchambers91@gmail.com>
2019-04-29 09:43:16 -0700

    restructured source code dir. .npmignore to ignore typescript files.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-exports.isPalindromePermutation = require(__dirname + '/lib/src/1-4-palindrome-permutation');
+exports.isPalindromePermutation = require(__dirname + '/src/1-4-palindrome-permutation');
 
 // ['taco cat', 'race car', 'racecar', 'nope', 'uh huh', 'what'].forEach(word => {
 //   console.log(`isPalindromePermutation(${word}) === ${isPalindromePermutation(word)}`);
